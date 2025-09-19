d82b42419a8001cb24d0700d1761108632c6bdb9
Tolykov Kirill <kirillmax@mail.ru>
2025-09-19 18:24:30 +0300

    Исправление конфликта

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -18,8 +18,6 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
